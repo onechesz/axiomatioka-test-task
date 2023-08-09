e8d2cba906f03ed8846c70863d77334f69ce295d
Ivan Minyaev <i.onechesz@gmail.com>
2023-08-09 14:28:10 +0300

    Initial commit

---
+++ src/main/java/com/github/onechesz/axiomatikatesttask/AxiomatikaTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.axiomatikatesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiomatikaTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxiomatikaTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/onechesz/axiomatikatesttask/AxiomatikaTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.axiomatikatesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxiomatikaTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
